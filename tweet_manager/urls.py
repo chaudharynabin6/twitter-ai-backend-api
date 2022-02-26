cb4a6a5f588fe6fc13bef5ece94766f7c1981033
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-02-26 11:02:03 +0545

    tweet_manager : analysed_tweet view
    * adding analysed_tweet view and url

--- tweet_manager/urls.py
+++ tweet_manager/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+
+
 from rest_framework import routers
-from .views import TweetReadOnlyViewSet
+from .views import TweetReadOnlyViewSet,analysed_tweet
 router = routers.DefaultRouter()
 
 router.register('tweet',viewset=TweetReadOnlyViewSet,basename='tweet')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path(r'analysed-tweets/<int:user>',view = analysed_tweet)
+]
--- tweet_manager/views.py
+++ tweet_manager/views.py
@@ -1,12 +1,51 @@
 from typing import Any
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import NotFound
+from django.shortcuts import get_object_or_404
+
+#local import
 from .models import Tweet
 from .serializers import TweetSerializer
-from rest_framework import viewsets
+from UserManager.models import TwitterUser
 
+# code starts
 class TweetReadOnlyViewSet(viewsets.ModelViewSet):
 
     def __init__(self, **kwargs: Any) -> None:
         self.queryset = Tweet.objects.all()
         self.serializer_class = TweetSerializer
         super().__init__(**kwargs)
+
+
+@api_view(['GET','PATCH'])
+def analysed_tweet(request,user=None):
+
+    if request.method == "GET":
+
+        if user != None:
+
+       
+            try:
+                twitter_user = TwitterUser.objects.get(user_id=user,isAnalysing=True)
+            
+            except TwitterUser.DoesNotExist : 
+                raise NotFound(detail={"analysing twitter user not found"})
+            # twitter_user = get_object_or_404(TwitterUser,user_id = user,isAnalysing = True)
+            analysed_tweets=Tweet.objects.filter(twitter_user=twitter_user,is_analysed=True)
+
+            serialized_tweets= TweetSerializer(instance=analysed_tweets,many=True)
+
+           
+
+            return Response(data=serialized_tweets.data,status=status.HTTP_200_OK)
+        else:
+            raise NotFound(detail={"pass user first"})
+            
+            
+            
+
+            
 
