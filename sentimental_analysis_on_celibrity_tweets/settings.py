204fc2c59c3263b0c26b1c9456fbad7fe81174d8
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-03-05 19:51:34 +0545

    Issue : OperationalError('database is locked')
    * solved

--- sentimental_analysis_on_celibrity_tweets/settings.py
+++ sentimental_analysis_on_celibrity_tweets/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+                    # ...
+                    'timeout': 20, # https://docs.djangoproject.com/en/dev/ref/databases/#database-is-locked-errorsoption
+                    # ...
+                }
     }
 }
 
